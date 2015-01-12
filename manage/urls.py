fd688206580f91c7fc37e686f1f156e640664b4e
ReanGD <reangd@yandex.ru>
2015-01-13 02:33:09 +0300

    create base struct

--- manage/urls.py
+++ manage/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from manage import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.main, name='main'),
+    url(r'^$', views.index, name='index'),
+    url(r'^films/$', views.films, name='films'),
+    url(r'^audiobook/$', views.audiobook, name='audiobook'),
 )
--- manage/views.py
+++ manage/views.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
-def main(request):
-    return render(request, 'manage/main.html')
+def index(request):
+    return redirect('/manage/films/')
+
+
+def films(request):
+    class Film(object):
+        def __init__(self, name):
+            self.name = name
+
+    items = [Film("one"), Film("two")]
+    return render(request, 'manage/films.html', {'items': items})
+
+
+def audiobook(request):
+    class Book(object):
+        def __init__(self, name):
+            self.name = name
+
+    items = [Book("first"), Book("second")]
+    return render(request, 'manage/audiobook.html', {'items': items})
